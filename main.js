b8b28c2fff8dd1881503f255776278f1ad5c0653
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-16 09:24:12 +0300

    1

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1/
+// function arrayFromAToB (StringArray){
+
+//      console.log(StringArray.sort());
+// }arrayFromAToB(['dfg','my','edsa','mmm'])
+// 2.
+// function getArrayFromSmallToBig (myArray){
+//     myArray.sort((a,b) => a-b)
+//     console.log(myArray);
+// }getArrayFromSmallToBig ([6,1,2,39,4]);
+function getArrayFromBigToSmall(myArray){
+ myArray.sort((b,a ) => a-b)
+ console.log(myArray);
+} getArrayFromBigToSmall([3,87,45,23,5,67,9])
