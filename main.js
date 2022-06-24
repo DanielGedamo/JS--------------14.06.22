e5acff24274786585eb9e8e8414446307d519a5f
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-24 18:26:24 +0300

    השלמתן

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
-// 1/
+// // 1/
 // function arrayFromAToB (StringArray){
-
-//      console.log(StringArray.sort());
-// }arrayFromAToB(['dfg','my','edsa','mmm'])
-// 2.
+// console.log(StringArray.sort());
+// }  (['dfg','my','edsa','mmm'])
+// // 2.
 // function getArrayFromSmallToBig (myArray){
 //     myArray.sort((a,b) => a-b)
 //     console.log(myArray);
 // }getArrayFromSmallToBig ([6,1,2,39,4]);
-function getArrayFromBigToSmall(myArray){
- myArray.sort((b,a ) => a-b)
- console.log(myArray);
-} getArrayFromBigToSmall([3,87,45,23,5,67,9])
+// function getArrayFromBigToSmall(myArray){
+//  myArray.sort((b,a ) => a-b)
+//  console.log(myArray);
+// } getArrayFromBigToSmall([3,87,45,23,5,67,9])
+// 4.
+// let myArray = [];
+// function getArrayFromAtoB() {
+//     if (myArray.length < 10){
+//   myArray.push( document.getElementById("userName").value);
+//   myArray.sort();
+//   document.getElementById("userName").value = " ";
+//     }
+// }getArrayFromAtoB()
+// // 5.
+// // let myArray = []
+// // function getRandomAge(){
+// //     for(i = 0; i< 10; i++ ){
+// //   myArray.push( Math.floor(Math.random()*111)+1).value
+// // }
+// //  console.log(myArray);
+// //  myArray.sort((a,b) => b-a)
+// //  return myArray
+// // }
+// // } getRandomAge()
+6.
+// function myArray2ToString(){
+//     let myStr = myArray.join("-")
+//     console.log(myStr);
+//     return myStr
+// }
+7.
+// function print10NameOfCompanys(myArray){
+// myArray.forEach(element => {
+//     document.write(element +'<br>')
+// });
+// }print10NameOfCompanys(["google",'dede','aaa','eee','bbb','xdx','sfre','ewwe','rfcvg','xvxv','fgr'])
+
+let picArray =[]
+function print12pics() {
+    
+}
+
+
+
